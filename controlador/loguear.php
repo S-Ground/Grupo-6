75f7bb4ac819f2fcc2691534f06468aa804658e9
S-Ground <gustavo.igor@inacapmail.cl>
2021-05-29 22:28:35 -0400

    Dashboard

--- controlador/loguear.php
+++ controlador/loguear.php
@@ -1,12 +1,14 @@
 <?php
+//inicio conexion a base de datos
 require 'db.php';
 session_start();
-
+//definicion de variables para login
 $usuario=$_POST['usuario'];
 $clave=$_POST['clave'];
 
 
-$q="SELECT COUNT(*) as contar from usuarios WHERE usuario='$usuario'and clave='$clave'";
+//consulta a la base de datos
+$q="SELECT   COUNT(*) as contar from usuarios WHERE usuario='$usuario'and clave='$clave'";
 
 $consulta=mysqli_query($conexion,$q);
 $array=mysqli_fetch_array($consulta);
@@ -14,9 +16,9 @@ $array=mysqli_fetch_array($consulta);
 
 if($array['contar']>0){
     $_SESSION['username']=$usuario;
-    header("location: ../app/view.php");
+    header("location: ../app/index.php");
 }else{
     echo "<br>";
-    echo"DATOS INCORRECTOS";
+    header("location: ../controlador/login.php");
 }
 ?>
