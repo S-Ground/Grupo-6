eab2cb710d2e75b04958c3d77a29f2bff58839e2
S-Ground <gustavo.igor@inacapmail.cl>
2021-06-07 11:55:10 -0400

    Docker

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2021 a las 04:23:04
+-- Tiempo de generación: 07-06-2021 a las 17:34:18
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 7.3.27
 
@@ -12,14 +12,11 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `php_login_database`
 --
+CREATE DATABASE IF NOT EXISTS `php_login_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `php_login_database`;
 
 -- --------------------------------------------------------
 
@@ -45,6 +42,3 @@ INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `apellido`, `nombre_organizac
 (2, '1', '1', '1', '1', '1');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
