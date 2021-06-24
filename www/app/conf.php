88731cbbedd5e5f24f8a179e40b76141c0e85321
S-Ground <gustavo.igor@inacapmail.cl>
2021-06-24 01:56:19 -0400

    avance

--- www/app/conf.php
+++ www/app/conf.php
@@ -148,20 +148,6 @@ if (!isset($usuario)) {
 
 
 
-  <!-- Core -->
-  <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
-  <script src="../js/cards.js"></script>
-  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/js-cookie/js.cookie.js"></script>
-  <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-  <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
-  <!-- Optional JS -->
-  <script src="assets/vendor/chart.js/dist/Chart.min.js"></script>
-  <script src="assets/vendor/chart.js/dist/Chart.extension.js"></script>
-  <!-- Argon JS -->
-  <script src="assets/js/argon.js?v=1.2.0"></script>
-  <!-- barra -->
-  <script src="./assets/js/bar.js"></script>
 </body>
 
 </html>
--- www/controlador/login.php
+++ www/controlador/login.php
@@ -6,7 +6,6 @@
     <title>Login</title>
     <link rel="stylesheet" href="../app/assets/css/bar.css">
     <link rel="stylesheet" href="../app/assets/css/log.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css%22%3E">
 </head>
 
 <body>
